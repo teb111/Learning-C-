ae10ead8cab0eb69a29370bd3160329050cc8542
teb111 <fakoredetobiloba.ft@gmail.com>
2022-12-12 04:19:33 +0100

    First stages

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Linq;
+using Catalog.Repositories;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Catalog.Entities;
+using Catalog.Dtos;
+
+namespace Catalog.Controllers
+
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepository repository;
+
+        public ItemsController(IItemsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // Get  /items
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItems()
+        {
+            var items = repository.GetItems().Select(item => item.AsDto());
+            return items;
+        }
+
+        // Get /items/{id}
+        [HttpGet("{id}")]
+
+        public ActionResult<ItemDto> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id).AsDto();
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+        //Post /items
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                createdDate = DateTimeOffset.UtcNow
+            };
+
+            repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
+        }
+
+        //Put /items/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            Item updatedItem = existingItem with
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+
+            repository.UpdateItem(updatedItem);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            repository.DeleteItem(id);
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemsRepository : IItemsRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item
+            {
+                Id = Guid.NewGuid(),
+                Name = "Potion",
+                Price = 9,
+                createdDate = DateTimeOffset.UtcNow
+            },
+
+            new Item
+            {
+                Id = Guid.NewGuid(),
+                Name = "Potion Black",
+                Price = 9,
+                createdDate = DateTimeOffset.UtcNow
+            },
+
+            new Item
+            {
+                Id = Guid.NewGuid(),
+                Name = "Tobiloba Extraordinary",
+                Price = 9,
+                createdDate = DateTimeOffset.UtcNow
+            }
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+
+        public void CreateItem(Item item)
+        {
+            items.Add(item);
+        }
+
+        public void UpdateItem(Item item)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == item.Id);
+            items[index] = item;
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            items.RemoveAt(index);
+        }
+    }
+}
